764ef4975b97110f98739e9ec1b70881aa5a4b5c
Dancetrap <hhs2791@g.rit.edu>
2025-07-20 19:37:27 -0400

    Started Package

---
+++ Assets/Scripts/Rabbyte/SbdFileHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+
+namespace Rabbyte
+{
+    public static class SbdFileHandler
+    {
+        //Used for converting bytes to string
+        /*
+         string convert = "This is the string to be converted";
+
+            // From string to byte array
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(convert);
+
+            // From byte array to string
+            string s = System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+         */
+    }
+}
