53e6ccaeeded388156f1b4e89debcc93a6a1158d
Dancetrap <hhs2791@g.rit.edu>
2025-09-10 11:33:29 -0400

    Update CloseHandler.cs

--- Assets/Scripts/Rabbyte/CloseHandler.cs
+++ Assets/Scripts/Rabbyte/CloseHandler.cs
@@ -5,6 +5,7 @@ using Rabbyte;
 public class CloseHandler
 {
     #region Editor Properties
+    #if UNITY_EDITOR
     /// This method is called when the script is loaded or a recompile occurs
     [InitializeOnLoadMethod]
     private static void OnInitialize()
@@ -21,6 +22,7 @@ public class CloseHandler
         // Add any specific actions or cleanup logic here,
         // such as saving data, releasing resources, etc.
     }
+    #endif
     #endregion
 
     #region Runtime Properties
