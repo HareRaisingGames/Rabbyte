aedf2c4a1808cb70b6c039970502eddf0dcd4064
Dancetrap <85971084+Dancetrap@users.noreply.github.com>
2026-01-26 11:38:42 -0500

    Update ReflectFunctions.cs

--- Assets/Scripts/Rabbyte/Gyokuto/ReflectFunctions.cs
+++ Assets/Scripts/Rabbyte/Gyokuto/ReflectFunctions.cs
@@ -23,6 +23,11 @@ public static partial class LuaMethods
     };
 
 #if NET_4_6
+    static FieldInfo GetField(string property)
+    {
+        
+        return null;
+    }
     /// <summary>
     /// Get the value of an object via string
     /// </summary>
@@ -31,7 +36,7 @@ public static partial class LuaMethods
     public static dynamic GetProperty(string property)
     {
         //Find the type object that this will be called out
-
+        //FieldInfo field = GetField(property);
         //return (dynamic)field.GetValue();
         return null;
     }
@@ -70,8 +75,8 @@ public static partial class LuaMethods
         { "Shake",  (Action<float, float, GameObject>)ShakeScreen },
         { "CameraShake",  (Action<float, float>)ShakeCamera },
         //Reflect Properties
-        { "GetProperty", (Func<string, dynamic>)GetProperty },
-        { "SetProperty", (Action<string, dynamic>)SetProperty },
+        //{ "GetProperty", (Func<string, dynamic>)GetProperty },
+        //{ "SetProperty", (Action<string, dynamic>)SetProperty },
         //Debug Properties
         { "DebugLog", (Action<object>)Read},
         { "DebugError", (Action<object>)Error},
@@ -98,23 +103,31 @@ public static partial class LuaMethods
     {
         if (!globals.ContainsKey(name))
             globals.Add(name, dyn);
+        else
+            globals[name] = dyn;
     }
     public static void AddGlobal(GlobalDictionary dic)
     {
         if (!globals.ContainsKey(dic.name))
             globals.Add(dic.name, dic.dyn);
+        else
+            globals[dic.name] = dic.dyn;
     }
     public static void AddGlobal(List<GlobalDictionary> dics)
     {
-        foreach(GlobalDictionary dic in dics)
+        foreach (GlobalDictionary dic in dics)
             if (!globals.ContainsKey(dic.name))
                 globals.Add(dic.name, dic.dyn);
+            else
+                globals[dic.name] = dic.dyn;
     }
     public static void AddGlobal(Dictionary<string, dynamic> newGlobals)
     {
-        foreach(KeyValuePair<string, dynamic> global in newGlobals)
+        foreach (KeyValuePair<string, dynamic> global in newGlobals)
             if (!globals.ContainsKey(global.Key))
                 globals.Add(global.Key, global.Value);
+            else
+                globals[global.Key] = global.Value;
     }
 }
 
